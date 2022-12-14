2882b4c1b280a6a7a8a35c3fde0e766094d17ee0
DESKTOP-AJM9JH3\sasch <sascha.frank.88@gmx.de>
2022-12-14 15:58:36 +0100

    config

--- src/flask_app/flask_login_tutorial/__init__.py
+++ src/flask_app/flask_login_tutorial/__init__.py
@@ -1,7 +1,35 @@
+from dotenv import load_dotenv
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from .config import Config
+import os
+from os import environ
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, ".env"))
+
+
+class Config:
+    """Set Flask configuration from environment variables."""
+
+    FLASK_APP = environ.get("FLASK_APP")
+    FLASK_ENV = environ.get("FLASK_ENV")
+    SECRET_KEY = environ.get("SECRET_KEY")
+
+    # Flask-SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///database.db")
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Flask-Assets
+    LESS_BIN = environ.get("LESS_BIN")
+    ASSETS_DEBUG = environ.get("ASSETS_DEBUG")
+    LESS_RUN_IN_DEBUG = environ.get("LESS_RUN_IN_DEBUG")
+
+    # Static Assets
+    STATIC_FOLDER = "static"
+    TEMPLATES_FOLDER = "templates"
+    COMPRESSOR_DEBUG = environ.get("COMPRESSOR_DEBUG")
+
 
 db = SQLAlchemy()
 login_manager = LoginManager()
