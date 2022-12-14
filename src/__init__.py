3b2b6fbeb52082d7aab3f5768cce575b528bbfa8
DESKTOP-AJM9JH3\sasch <sascha.frank.88@gmx.de>
2022-12-14 15:18:26 +0100

    rel import

---
+++ src/__init__.py
@@ -0,0 +1,36 @@
+import os
+from flask import request, jsonify
+from src.flask_app.wsgi import create_app
+from src.wyl import validate_post_data
+
+app = create_app()
+app.secret_key = os.urandom(32)
+
+
+@app.route('/api', methods=['GET', 'POST'])
+def api():
+    """
+    /api entpoint
+    GET - returns json= {'status': 'test'}
+    POST -  {
+            name - str not null
+            age - int optional
+            }
+    :return:
+    """
+    if request.method == 'GET':
+        return jsonify({'status': 'test'})
+    elif request.method == 'POST':
+        if validate_post_data(request.json):
+            return jsonify({'status': 'OK'})
+        else:
+            return jsonify({'status': 'bad input'}), 400
+
+
+def main():
+    app.run(host='0.0.0.0', port=8080)
+
+
+if __name__ == '__main__':
+    print(app.url_map)
+    main()
