719da2ad65f995976f7e45593972731178b8c489
DESKTOP-AJM9JH3\sasch <sascha.frank.88@gmx.de>
2022-12-14 15:38:59 +0100

    imports

--- src/flask_app/flask_login_tutorial/__init__.py
+++ src/flask_app/flask_login_tutorial/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from src.flask_app.config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
