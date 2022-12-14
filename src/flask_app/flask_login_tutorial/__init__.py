04e99ffc59aa9b06ff6b3547fcdc38400d7b1eb0
DESKTOP-AJM9JH3\sasch <sascha.frank.88@gmx.de>
2022-12-14 15:48:27 +0100

    config

--- src/flask_app/flask_login_tutorial/__init__.py
+++ src/flask_app/flask_login_tutorial/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-import config
+from .config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -9,7 +9,7 @@ login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object("config")
+    app.config.from_object("Config")
 
     db.init_app(app)
     login_manager.init_app(app)
